e1ea923ad709d45fd057231ff164574d80a13f7e
markovskyyyyy <marcosanchez@alumno.unlam.edu.ar>
2021-10-07 09:29:06 -0300

    modifique agua

--- src/dominio/Agua.java
+++ src/dominio/Agua.java
@@ -1,5 +1,8 @@
 package dominio;
 
 public class Agua extends Bebida {
+	 public Agua(Double cantidad, Double precio, String marca, Integer id) {
+	        super(cantidad, precio, marca, id);
 
+	    }
 }
