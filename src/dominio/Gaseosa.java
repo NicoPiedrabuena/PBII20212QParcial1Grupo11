a0acb91b2d3c95cec3a806ce163c14b2848d1116
markovskyyyyy <marcosanchez@alumno.unlam.edu.ar>
2021-10-07 09:45:10 -0300

    agregue Gaseosa

---
+++ src/dominio/Gaseosa.java
@@ -0,0 +1,39 @@
+package dominio;
+
+public class Gaseosa extends Bebida {
+
+    private Double porcentajeAzucar;
+    private Double promocion;
+
+    private TipoGaseosa tipoDeGaseosa;
+
+
+    // RE VER LA PROMOCION
+
+    public Gaseosa(Double cantidad, Double precio, String marca, Integer id, Double porcentajeAzucar) {
+        super(cantidad, precio, marca, id);
+        this.porcentajeAzucar = porcentajeAzucar;
+        // this.promocion = promocion;   REE VERLO
+    }
+
+
+
+    public Double getPorcentajeAzucar() {
+        return porcentajeAzucar;
+    }
+
+    public void setPorcentajeAzucar(Double porcentajeAzucar) {
+        this.porcentajeAzucar = porcentajeAzucar;
+    }
+
+    // calcular promocion de cada bebida
+    public Double getPromocion(String marca) {
+        return promocion;
+    }
+
+    public void setPromocion(Double promocion) {
+        this.promocion = promocion;
+    }
+
+}
+
